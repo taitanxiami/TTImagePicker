5e8f0d1b6d60c5ed09dae7f137827c4d639b8201
taitanxiami <taitanxiami@gmail.com>
2017-04-28 10:49:16 +0800

    Initial Commit

---
+++ TTImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTImagePicker
+//
+//  Created by dianda on 2017/4/28.
+//  Copyright © 2017年 taitanxiami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTImagePicker
+//
+//  Created by dianda on 2017/4/28.
+//  Copyright © 2017年 taitanxiami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
